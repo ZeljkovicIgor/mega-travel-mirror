93d98551c15739c2beeb2d66281441fbdf2d1d23
Iva <anticivana27@gmail.com>
2019-06-07 23:21:06 +0200

    IZMENE

--- main-backend/src/main/java/com/megatravel/mainbackend/service/ServiceServiceImpl.java
+++ main-backend/src/main/java/com/megatravel/mainbackend/service/ServiceServiceImpl.java
@@ -3,7 +3,7 @@ package com.megatravel.mainbackend.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 
 import com.megatravel.mainbackend.repository.ServiceRepository;
 
