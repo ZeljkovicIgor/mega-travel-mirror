5d8e3eaa6da236a5872b3d12bf2caa744040ce79
Gema <gemovics@gmail.com>
2019-06-28 21:57:54 +0200

    converter

--- agent-module/agent-backend/src/main/java/com/megatravel/agentbackend/service/SoapService.java
+++ agent-module/agent-backend/src/main/java/com/megatravel/agentbackend/service/SoapService.java
@@ -1,12 +1,19 @@
 package com.megatravel.agentbackend.service;
 
-import com.megatravel.agentbackend.model.Accommodation;
-import com.megatravel.agentbackend.model.Reservation;
-import com.megatravel.agentbackend.model.User;
+import com.megatravel.agentbackend.model.*;
+import com.megatravel.agentbackend.ws.AccTypeSoap;
+import com.megatravel.agentbackend.ws.AddServiceSoap;
+import com.megatravel.agentbackend.ws.CategorySoap;
+
+import java.util.List;
 
 public interface SoapService {
 
-    void logInSync(User agent);
+    void logInSync(String username, String password);
+    List<User> getUsers(User agent);
+    List<AccTypeSoap> getAccTypes(User agent);
+    List<CategorySoap> getCategories(User agent);
+    List<AddServiceSoap> getAddServices(User agent);
     Accommodation sendOneAccommodation(Accommodation accommodation);
     Reservation sendReservation(Reservation reservation);
 }
